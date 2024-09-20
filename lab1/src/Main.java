e92cbbfdb094cc966b15d4b2aecc83afe2121824
Keallan52 <rozanov.05@list.ru>
2024-09-20 20:45:48 +0300

    a

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i <= 300; i++) {
+            if (i % 4 == 0 && i % 6 != 0 && sumDigits(i) < 10) {
+                System.out.print(i + " ");
+            }
         }
+    }
+
+    public static int sumDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
     }
 }
